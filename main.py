1d432f8110230ed30f4f7d22727c44386061b304
TheSausageSwiper <97609792+onenicktwo@users.noreply.github.com>
2023-09-30 17:52:15 -0500

    Setup main and removed empty file

--- main.py
+++ main.py
@@ -3,36 +3,27 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import pickle
 
-#weather = pd.read_csv("iowa_weather_data.csv")
-#weather.columns = weather.columns.str.lower()
+weather = pd.read_csv("iowa_weather_data.csv")
+weather.columns = weather.columns.str.lower()
 
-#weather['date'] = pd.to_datetime((weather['date']))
-#weather['year'] = weather['date'].apply(lambda x : x.year)
+weather['date'] = pd.to_datetime((weather['date']))
+weather['year'] = weather['date'].apply(lambda x : x.year)
+weather = weather[weather['year'] >= 2010]
 
 #plt.plot(weather['date'], weather['tmax'])
 #plt.show()
 
 #Data
-#data = weather[['date', 'tmin']]
-#data.dropna(inplace=True)
-#data.columns = ['ds', 'y']
-
-#Train
-#m = NeuralProphet()
-#m.fit(data, freq='D', epochs=1000)
-#m.restore_trainer()
-
-#with open('iowa_weather_model.pkl', 'wb') as f:
-#    pickle.dump(m, f)
+data = weather[['date', 'tmin']]
+data.dropna(inplace=True)
+data.columns = ['ds', 'y']
 
 #Forecast
 with open('iowa_weather_model.pkl', 'rb') as f:
     m = pickle.load(f)
 
-print(m)
+future = m.make_future_dataframe(data, periods=365)
+forecast = m.predict(future)
 
-#future = m.make_future_dataframe(data, periods=365)
-#forecast = m.predict(future)
-
-#plt.plot(forecast['ds'],forecast['yhat1'])
-#plt.show()
+plt.plot(forecast['ds'],forecast['yhat1'])
+plt.show()
